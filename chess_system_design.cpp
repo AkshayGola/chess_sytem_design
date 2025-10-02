a23e3931409d58d2de90b3d41ab29b61ffed3ed2
Akshay Gola <akshay.gola@yahoo.in>
2025-10-02 08:26:13 +0530

    added game and piece classes. TBD: setting the status of the game appropriately.

--- chess_system_design.cpp
+++ chess_system_design.cpp
@@ -1,31 +1,48 @@
+using namespace std;
+
 class block {
     int x;
     int y;
+    piece * p;
     public:
         block(int x, int y)
         {
             this->x = x;
             this->y = y;
+            this->p = NULL;
         }
 };
 
 class player {
     private:
         bool iswhite;
+        bool isturn;
 };
 
-
-
-class Piece {
+class piece {
     private:
-        bool iswhite;
+        bool is_white;
+        bool is_alive;
 
     public:
+        piece ()
+        {
+            this->is_white = true;
+            this->is_alive = true;
+        }
+        void set_is_white(bool is_white)
+        {
+            this->is_white = is_white;
+        }
+        bool get_is_white()
+        {
+            return is_white;
+        }
         virtual bool can_move(block start, block end) = 0;
 };
 
 
-class king extends Piece {
+class king extends piece {
     public:
         bool can_move(block b1, block b2) override {
             if ((b1.x - b2.x) <= 1 && (b1.y - b2.y) <= 1)
@@ -39,7 +56,7 @@ class king extends Piece {
         }
 };
 
-class queen extends Piece {
+class queen extends piece {
     public:
         bool can_move(block b1, block b2) override {
             if (((b1.x - b2.x) == (b1.y - b2.y)) || (b1.x - b2.x) == 0 || (b1.y - b2.y) == 0)
@@ -51,4 +68,51 @@ class queen extends Piece {
                 return false;
             }
         }
+};
+
+class game {
+    board block[8][8];
+    player p1;
+    player p2;
+    int status;
+    int turn;
+    bool make_move(player p, block b1, block b2)
+    {
+        if (player p.isturn == false)
+        {
+            cout << "Invalid Move" << '\n';
+            return;
+        }
+
+        if (b1.peice == NULL)
+        {
+            cout << "Invalid Move" << '\n';
+            return;
+        }
+
+        if (b1.peice.can_move(b1, b2) == false)
+        {
+            cout << "Invalid Move" << '\n';
+            return false;
+        }
+
+        if (b2.piece.iswhite == NULL)
+        {
+            b1.piece = NULL;
+            b2.piece = p;
+        }
+
+        if (b2.piece.iswhite == p.iswhite)
+        {
+            cout << "Invalid Move" << '\n';
+            return false;
+        }
+        else
+        {
+            b1.piece = NULL;
+            b2.piece.is_alive = false;
+            b2.piece = p;
+        }
+
+    }
 };
